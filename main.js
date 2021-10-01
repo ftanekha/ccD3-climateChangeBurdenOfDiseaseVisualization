d6488499f29c1b556e52e48e429655a11a85209f
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-10-01 19:36:57 +0100

    add interactivity

--- main.js
+++ main.js
@@ -8,12 +8,6 @@ const climate_daly_data = [
     {region: 'High income countries', deaths: 23}
 ]
 
-
-const toggleClass = (i,toggle) => {
-    d3.select('#viz div:nth-child('+ i +')').classed('highlightBar',toggle);
-    d3.select('#legend li:nth-child('+ i +')').classed('highlightText',toggle);
-};
-
 //1 - select DOM to bind data to
 const divSelection = d3.select('div#viz')
 divSelection.selectAll('div')
@@ -30,7 +24,6 @@ divSelection.selectAll('div')
         d => `${d.deaths * 20}px`
     )
 
-
 //5 - create legend list
 const listSelection = d3.select('ol#legend')
 listSelection.selectAll('li')
@@ -40,3 +33,39 @@ listSelection.selectAll('li')
     //6 - add text to generate list items
     .text(d => `${d.region}: ${d.deaths}`)
 
+//7 - select both the bar chart and the corresponding legend element with d3 by 
+//accessing the nth child of both the #legend and #viz elements. 
+//use the .classed() method to remove and apply the styles to the corresponding elements
+const toggleClass = (i,toggle) => {
+    d3.select('#viz div:nth-child('+ i +')').classed('highlightBar',toggle)
+    d3.select('#legend li:nth-child('+ i +')').classed('highlightText',toggle)
+}
+
+// listSelection.on(
+//     'mouseover',
+//     (d, i) => toggleClass(i + 1, true)
+// )
+// listSelection.on(
+//     'mouseout',
+//     (d, i) => toggleClass(i + 1, false)
+// )
+
+//**** due to 'breaking change 'in the D3 library code for events, the above don't work so let's go old school on dat ass
+
+//querySelector().children returns a HTMLCollection Object, NOT an Array, so indexOf doesn't work
+//so copy data from the HTMLCollection Object to arrays
+const listItems = [],
+divItems = []
+for(let item of document.querySelector('ol').children) listItems.push(item)
+for(let div of document.querySelector('div#viz').children) divItems.push(div)
+
+for(let item of listItems){
+    item.addEventListener('mouseenter', () => {
+        item.classList.add('highlightText')
+        divItems[listItems.indexOf(item)].classList.add('highlightBar')
+    })
+    item.addEventListener('mouseleave', () => {
+        item.classList.remove('highlightText')
+        divItems[listItems.indexOf(item)].classList.remove('highlightBar') 
+    })
+}
